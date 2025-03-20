a4faa81d303aaee2500c188d1a317282931f8b72
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-03-20 00:05:51 +0000

    yay

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,40 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int find(int[] array, int number) {
+		for (int i = 0; i < array.length; i++){
+			if (array[i] == number) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int findLast(String[] array, String string) {
+		for (int i = array.length - 1; i >= 0; i--){
+			if (array[i].equals(string)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int findSecond(char[] array, char chracter) {
+		int count = 0; 
+		int index = 0; 
+		for (int i = 0; i < array.length; i++){
+			if (array[i] == chracter) {
+				count ++;
+				index = i;
+				if (count == 2){
+					return index; 
+				}
+			}
+		}
+		if (count == 1){
+			return index;
+		}
+		else {
+			return -1;
+		}
+	}
 
 }
